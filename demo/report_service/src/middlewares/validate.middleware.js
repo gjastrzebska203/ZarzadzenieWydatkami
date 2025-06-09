051244264475cc0506afc81b3d8f3a0353d21c17
Gabriela <gabrysia@Mac.home>
2025-06-09 18:00:07 +0200

    ...

--- demo/report_service/src/middlewares/validate.middleware.js
+++
@@ -1,8 +0,0 @@
-const { body } = require('express-validator');
-
-const validateCreateReport = [
-  body('from').isDate().withMessage('Błąd typu.'),
-  body('to').isDate().withMessage('Błąd typu.'),
-];
-
-module.exports = { validateCreateReport };
--- demo/user_service/src/models/index.js
+++
@@ -1,9 +0,0 @@
-const { sequelize } = require('../config/db');
-const UserModel = require('./user.model');
-
-const User = UserModel(sequelize);
-
-module.exports = {
-  sequelize,
-  User,
-};
